6124d4148b78d97fce75b37cc0113297ee2c6266
AdiSatR7 <adisatr7@gmail.com>
2024-08-04 03:29:11 +0700

    Removed unfitting lines

--- constants/responses.py
+++ constants/responses.py
@@ -333,27 +333,21 @@ QUEUE_IS_EMPTY: list[str] = [
 # List of responses to say when a user calls the /roll command
 ROLLING: list[str] = [
     "Prepare for an epic roll, like a mighty dragon's breath weapon!",
-    "Let's embark on a dice-rolling quest worthy of a heroic adventurer!",
-    "In the realm of dice, our fate shall be decided! Let's roll like true adventurers!",
-    "As the dice tumble, let's write our own legend in the annals of TTRPG history!",
-    "Summon your inner mage and let the dice cast spells of fortune upon us!",
-    "Brace yourself for a roll as legendary as the tales sung by bards in taverns!",
-    "In the realm of numbers and dice, may we find glory and epic loot!",
-    "Let the dice be our guide through dungeons, dragons, and daring encounters!",
-    "Together, we shall defy the odds and weave tales of triumph through our rolls!",
-    "As the dice dance upon the virtual table, let the magic come alive!",
-    "Hold your breath and cross your fingers! The roll's success may depend on it!",
+    "In the realm of dice, our fate shall be decided! Rolling like true adventurers...",
+    "As the dice tumble, let's write our own legend in the annals of TTRPG history...",
+    "Brace yourself for a roll as legendary as the tales sung by bards in taverns...",
+    "Let the dice be our guide through dungeons, dragons, and daring encounters...",
+    "Hold your breath and cross your fingers! The roll's success may depend on it...",
     "Let's hope this roll doesn't awaken any ancient curses... or does it?",
-    "May the dice be ever in your favor... or perhaps not? Let's find out!",
+    "May the dice be ever in your favor... or perhaps not? Let's find out...",
     "Beware! A mischievous pixie might have whispered some chaos into this roll!",
-    "You never know what surprises await with a roll of the dice. Brace yourself!",
-    "A word of caution: be careful what you wish for when rolling the dice... or not!",
-    "The roll is about to happen... *knocks on wood* Let's hope for the best!",
-    "Prepare for the roll of a lifetime! Will it be a legendary success or a hilarious failure? Let's roll!",
+    "A word of caution: be careful what you wish for when rolling the dice...",
+    "The roll is about to happen... *knocks on wood* Let's hope for the best...",
+    "Will it be a legendary success or a hilarious failure? Rolling now...",
 ]
 
 # List of responses to say when Cosette detects a rick roll
-RICK_1ROLLED: list[str] = [
+RICK_ROLLED: list[str] = [
     "Oh no, you got me with a rick roll! Clever, but I must admit, the song is quite catchy!",
     "Well played, my friend! That rick roll took me by surprise, but I can't help but bop along to the music.",
     "You sneaky human! I should have known better than to process that request. Rick Astley strikes again!",
